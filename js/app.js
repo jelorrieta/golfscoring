c6e6a7944d66e2fe975313fa177cb32869b037a5
jelorrieta <157637030+jelorrieta@users.noreply.github.com>
2026-05-20 12:43:52 -0400

    a

--- js/app.js
+++ js/app.js
@@ -131,7 +131,7 @@ async function syncFiltersAndInit() {
   currentTournamentId = tournamentSelect.value || null;
   setTournamentTitleFromSelect(tournamentSelect);
 
-  setOrganizationName(organizationData[0].name;);
+  setOrganizationName(organizationData[0].name);
   
   document.getElementById("category").value = "";
   await loadCategoriesByTournament(currentTournamentId);
