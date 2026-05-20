6de363c70c80a215061ea93158915b8dd5098ba4
jelorrieta <157637030+jelorrieta@users.noreply.github.com>
2026-05-20 11:00:17 -0400

    a

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ async function loadInitialData() {
   const hostname = window.location.hostname;
   console.log(hostname);
   const { data } = await supabase.rpc('get_organization_by_hostname',{ p_hostname: hostname });
-  const organizationId = data.id;
+  const organizationId = data[0].id;
   console.log(data);
   const { data: tournaments } = await supabase.rpc(
     'get_tournaments',
