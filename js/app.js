5a557eecbc54990a0fe0ea797bb75d254ab1108c
jelorrieta <157637030+jelorrieta@users.noreply.github.com>
2026-05-23 10:42:19 -0400

    a

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import { supabase } from './supabaseClient.js';
-import { renderScorecardInElement } from './scorecard.js?v8';
+import { renderScorecardInElement } from './scorecard.js?v9';
 
 
 // =============================
