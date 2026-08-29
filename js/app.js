0f7636f76095c27476882d85a7263a8a4a6662bb
jelorrieta <157637030+jelorrieta@users.noreply.github.com>
2026-08-28 23:47:34 -0400

    Change formatName condition from 'gross' to 'dobles'

--- js/app.js
+++ js/app.js
@@ -279,7 +279,7 @@ function buildLeaderboardTable(data, { sortBy }) {
   const rowsHtml = data.map((row, index) => {
     let name = row.player_name;
     
-    if (formatName === 'scramble' || formatName === 'gross') {
+    if (formatName === 'scramble' || formatName === 'dobles') {
       name =
       ` <div class="player">${row.player_name}</div>
         <div class="guest">${row.guest}</div>`;
